53e16ea96edf2131775de0d651933267d45ce213
Sharjeel Mustafa <sharjeeliv@gmail.com>
2023-03-13 23:24:45 -0400

    removed sub module

--- app.py
+++ app.py
@@ -1,34 +1,53 @@
-from flask import Flask, render_template, flash, request, redirect, url_for, logging
-
 import os
+import gitmodules
 
+from flask import Flask, render_template, flash, request, redirect, url_for, logging, make_response
 from werkzeug.utils import secure_filename
 
+
+
 UPLOAD_FOLDER = 'storage'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'pdf'}
 
 app = Flask(__name__)
-app.config['UPLOAD_PATH'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 logging.create_logger(app)
 
 
 @app.route('/')
-def home():  # put application's code here
-    app.logger.info("Activated")
-    return render_template("main.html")
-
+def landing():  # put application's code here
+    cookie_input = False if request.cookies.get("lib-key") else True
+    # Need to add a security layer
+    return render_template("main.html", cookie_input=cookie_input)
 
 @app.route('/', methods=['POST'])
-def upload_file():
-    app.logger.info("Activated")
-    files = request.files.getlist('file[]')
-    for file in files:
-        print(file.filename)
-    # print(request.files)
-    # if uploaded_file.filename != '':
-    #     # uploaded_file.save(uploaded_file.filename)
-    #     uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], uploaded_file.filename))
-    return redirect(url_for('home'))
+def landing_input():
+
+    # Check and Set Lib Key Cookies
+    text = request.form.get("cookie")
+    if text:
+        app.logger.info(f'{text}')
+        res = make_response(redirect(url_for('landing')))
+        res.set_cookie("lib-key", text)
+        return res
+
+    # Check and Set Files
+    input_files = request.files.getlist('file[]')
+    if not input_files: return redirect(url_for('landing'))
+    files = []
+
+    for file in input_files:
+        if not allowed_file(file.filename): continue
+        filename = secure_filename(file.filename)
+        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(path)
+        files.append(path)
+
+    return redirect(url_for('landing'))
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 if __name__ == '__main__':
